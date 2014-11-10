3bd9afe7eaf51d742aafd16023a12d03ea9212b1
alfeo4 <alfeo4@gmail.com>
2014-11-10 11:55:45 +0100

    testing things

--- Pinball/main.cpp
+++ Pinball/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char* argv[])
@@ -6,7 +7,8 @@ int main(int argc, char* argv[])
 	sf::CircleShape circle(20.f);
 	circle.setPointCount(500);
 	circle.setFillColor(sf::Color::Green);
-	
+
+
 	sf::RectangleShape rectangle(sf::Vector2f(600, 400));
 	rectangle.setFillColor(sf::Color::White);
 
